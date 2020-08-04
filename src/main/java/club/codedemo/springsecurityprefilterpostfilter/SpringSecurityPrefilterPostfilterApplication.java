323d55775aa99946dc4a88ac2a5eb31498c7228e
panjie <3792535@qq.com>
2020-08-04 17:10:47 +0800

    init

---
+++ src/main/java/club/codedemo/springsecurityprefilterpostfilter/SpringSecurityPrefilterPostfilterApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.springsecurityprefilterpostfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPrefilterPostfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPrefilterPostfilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/springsecurityprefilterpostfilter/SpringSecurityPrefilterPostfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.springsecurityprefilterpostfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPrefilterPostfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
